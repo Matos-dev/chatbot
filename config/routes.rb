462cf022813972b7795610dfeda3f716974a1e88
Matos <eliuvism@gmail.com>
2022-06-04 21:09:43 -0400

    Update root url.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'chatbot_intents/index', to: 'chatbot_intents#index'
+  get '/', to: 'chatbot_intents#index'
   get 'chatbot_intents/consult_deposit', to: 'chatbot_intents#consult_deposit'
   get 'chatbot_intents/consult_indicators', to: 'chatbot_intents#consult_indicators'
   get 'chatbot_intents/deposit_details', to: 'chatbot_intents#deposit_details'
